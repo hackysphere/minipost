1178d79554819cec78474d098c51ec84ae27445d
hackysphere <hi@hcks.dev>
2026-04-17 14:16:43 -0600

    set up a default config and fix tests

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,3 @@
-import sys
 import tomllib
 from typing import TypedDict, cast
 
@@ -16,29 +15,34 @@ class ConfigFileTypes(TypedDict):
     BEHIND_PROXY: bool
 
 
+defaultconfig = {
+    "USERNAME_MIN_CHARS": 4,
+    "USERNAME_MAX_CHARS": 24,
+    "POST_MAX_CHARS": 300,
+    "USER_MAX_POSTS": 15,
+    "RATE_LIMIT": 500,
+    "RATE_LIMIT_WINDOW": 60,
+    "BEHIND_PROXY": False,
+}
+
 try:
     # binary mode because that's how tomllib works
     with open("config.toml", "rb") as file:
-        # using cast makes type checking assume that the Any part from the file will still adhere to the type schema
-        # otherwise ty will give type error and that will be annoying to deal with
-        # (maybe this isn't the best way to do this and a type check ignore would be better?)
-        config = cast(ConfigFileTypes, tomllib.load(file))
+        fileconfig = tomllib.load(file)
 except FileNotFoundError:
-    raise sys.exit(
-        "No config file found, please use the example given or create your own."
-    )
+    print("No config file found, using defaults...")
+    fileconfig = {}
 
+# using cast makes type checking assume that the Any part from the file will still adhere to the type schema
+# otherwise ty will give type error and that will be annoying to deal with
+# (maybe this isn't the best way to do this and a type check ignore would be better?)
+config = cast(ConfigFileTypes, defaultconfig | fileconfig)
 
-try:
-    # this could be improved, but this lets me not refactor for now
-    USERNAME_MIN_CHARS = config["USERNAME_MIN_CHARS"]
-    USERNAME_MAX_CHARS = config["USERNAME_MAX_CHARS"]
-    POST_MAX_CHARS = config["POST_MAX_CHARS"]
-    USER_MAX_POSTS = config["USER_MAX_POSTS"]
-
-    RATE_LIMIT = config["RATE_LIMIT"]  # requests per time window
-    RATE_LIMIT_WINDOW = config["RATE_LIMIT_WINDOW"]  # time window for requests
-
-    BEHIND_PROXY = config["BEHIND_PROXY"]
-except KeyError as err:
-    sys.exit(f"Missing key: {err}")
+# this could be improved, but this lets me not refactor for now
+USERNAME_MIN_CHARS = config["USERNAME_MIN_CHARS"]
+USERNAME_MAX_CHARS = config["USERNAME_MAX_CHARS"]
+POST_MAX_CHARS = config["POST_MAX_CHARS"]
+USER_MAX_POSTS = config["USER_MAX_POSTS"]
+RATE_LIMIT = config["RATE_LIMIT"]  # requests per time window
+RATE_LIMIT_WINDOW = config["RATE_LIMIT_WINDOW"]  # time window for requests
+BEHIND_PROXY = config["BEHIND_PROXY"]
